a0aacf813afd3ed120d48ccfb03092a81960e457
Matheus Monthay <99937232+MatheusMonthay@users.noreply.github.com>
2022-10-23 17:10:15 -0400

    FINAL

---
+++ README.md
@@ -0,0 +1,1 @@
+## Desafio do bootcamp da Dio - Abstraindo um Bootcamp Usando Orientação a Objetos em Java
--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,44 @@ public class Main {
         curso1.setTitulo("java");
         curso1.setDescricao("descricao do java");
         curso1.setCargaHoraria(50);
-        System.out.println(curso1);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("js");
+        curso2.setDescricao("descricao do js");
+        curso2.setCargaHoraria(80);
 
         Mentoria mentoria = new Mentoria();
-        mentoria.setTitulo("poo");
+        mentoria.setTitulo("POO");
         mentoria.setDescricao("descricaco mentoria");
         mentoria.setData(LocalDate.now());
-        System.out.println(mentoria);
+
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devMatheus = new Dev();
+        devMatheus.setNome("Matheus");
+        devMatheus.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Matheus:" + devMatheus.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Matheus:" + devMatheus.getConteudosConcluidos());
+        System.out.println("XP:" + devMatheus.calcularTotalXp());
+        System.out.println("-");
+
+        Dev devCamila = new Dev();
+        devCamila.setNome("Camila");
+        devCamila.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Camila:" + devCamila.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Camila:" + devCamila.getConteudosConcluidos());
+        devCamila.progredirBootcamp();
+        devCamila.progredirBootcamp();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Camila:" + devCamila.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Camila:" + devCamila.getConteudosConcluidos());
+        System.out.println("XP:" + devCamila.calcularTotalXp());
     }
 
     
